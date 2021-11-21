475975c12fe09b97906da260129ee27b7c0dfa3b
harmo00ny <harm000ny1225@gmail.com>
2021-11-21 15:49:10 +0900

    リモート追加

---
+++ app/src/main/java/com/example/compose_github_preview_app/data/remote/GithubApi.kt
@@ -0,0 +1,15 @@
+package com.example.compose_github_preview_app.data.remote
+
+import com.example.compose_github_preview_app.model.Repository
+import com.example.compose_github_preview_app.model.User
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GithubApi {
+    @GET("/users/{user}")
+    suspend fun getUser(@Path("user") user: String): Response<User>
+
+    @GET("users/{user}/repos?per_page=100")
+    suspend fun getUserRepository(@Path("user") user: String): Response<List<Repository>>
+}
