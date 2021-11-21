226548a4fd91aa8130c850393265195ead8090a2
harmo00ny <harm000ny1225@gmail.com>
2021-11-21 15:55:07 +0900

    エントリーポイント追加

--- app/src/main/java/com/example/compose_github_preview_app/MainActivity.kt
+++ app/src/main/java/com/example/compose_github_preview_app/MainActivity.kt
@@ -1,38 +1,43 @@
 package com.example.compose_github_preview_app
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.compose_github_preview_app.ui.screen.home.HomeScreen
 import com.example.compose_github_preview_app.ui.theme.Compose_GitHub_Preview_AppTheme
+import com.orhanobut.logger.Logger
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Logger.i("Launch")
         setContent {
             Compose_GitHub_Preview_AppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                App()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Compose_GitHub_Preview_AppTheme {
-        Greeting("Android")
+fun App() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") { HomeScreen(navController, hiltViewModel()) }
+        composable(
+            "web-view/?url={url}",
+            arguments = listOf(navArgument("url") { type = NavType.StringType }),
+        ) { backStackEntry ->
+//            WebViewScreen(url = backStackEntry.arguments?.getString("url") ?: "")
+        }
     }
 }
